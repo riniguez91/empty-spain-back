aa6c51001335aadffb325bb2d419b212df346875
victor00hs <46093788+victor00hs@users.noreply.github.com>
2021-06-11 21:38:52 +0200

    Seeder user_busqueda added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([SqlSeeder::class, UserSeeder::class, BusquedaSeeder::class]);
+        $this->call([SqlSeeder::class, UserSeeder::class, BusquedaSeeder::class, UserBusquedaSeeder::class]);
     }
 }
